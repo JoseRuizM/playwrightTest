b7b94ae2f575945874d03408e9269647308b6e6d
JoseRuizM <joseandfitch@gmail.com>
2023-06-04 23:03:22 -0600

    Changes on the link locator string

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,20 +1,24 @@
 //@ts-check
 import {test, expect} from '@playwright/test';
 
-test('has title', async ({page})=>{
-    await page.goto('https://playwright.dev/');
 
-    //Expect a title "to Contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+test.describe('First group of tests', ()=>{
+    test('has title', async ({page})=>{
+        await page.goto('https://playwright.dev/');
+    
+        //Expect a title "to Contain" a substring.
+        await expect(page).toHaveTitle(/Playwright/);
+    });
+    
+    test('get started link', async ({page})=>{
+        await page.goto('https://playwright.dev/');
+    
+        //click the get started link.
+        await page.getByRole('link', {name: 'Get tarted'}).click();
+    
+        //Expects the URL to contain intro.
+        await expect(page).toHaveURL(/.*intro/);
+    });
 });
 
-test('get started link', async ({page})=>{
-    await page.goto('https://playwright.dev/');
-
-    //click the get started link.
-    await page.getByRole('link', {name: 'Get started'}).click();
-
-    //Expects the URL to contain intro.
-    await expect(page).toHaveURL(/.*intro/);
-})
 
